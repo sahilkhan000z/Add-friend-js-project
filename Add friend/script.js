f2b697cfdfd035f201a19df38df9ac0842763bf3
sahilkhan000z <sahilkhan000zx@gmail.com>
2026-01-17 20:27:01 +0530

    actual add me project

---
+++ Add friend/script.js
@@ -0,0 +1,30 @@
+var istatus = document.querySelector("h5");
+var btn = document.querySelector("#add")
+var check = 0;
+
+
+btn.addEventListener("click", function(){
+    if(check==0){
+        istatus.innerHTML = "friend";
+        istatus.style.color = "green";
+        btn.innerHTML = "Remove Friend";
+        check = 1;
+    }
+    else{
+        istatus.innerHTML = "stranger";
+        istatus.style.color = "red";
+        btn.innerHTML = "Add friend";
+        check = 0;
+    }
+    
+})
+
+
+
+
+
+
+
+
+
+
